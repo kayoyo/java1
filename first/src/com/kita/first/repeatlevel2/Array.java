7b4e1dfbb7d441e0dcb3c6ecd1c5573c70fa292c
kayoyo <66930491+kayoyo@users.noreply.github.com>
2020-08-26 19:52:12 +0900

    Update Array.java

--- first/src/com/kita/first/repeatlevel2/Array.java
+++ first/src/com/kita/first/repeatlevel2/Array.java
@@ -57,11 +57,7 @@ public class Array {
 				}
 				
 		}
-		int arr[][] = new int[6][6]
-			for(int i=0; i<arr.length; i++){
-				for(int j=0; j<arr[i].length; j++){
-					if(arr[])
-			}
+		
 		}	
 		
 	}
